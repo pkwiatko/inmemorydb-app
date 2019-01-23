e7136b5272e7547689afbf563a7b843f81b24eb3
Pawel <kwiatp@gmail.com>
2019-01-23 14:14:19 +0100

    Initial import

---
+++ src/main/java/com/pkwiatko/inmemorydbapp/InmemorydbAppApplication.java
@@ -0,0 +1,14 @@
+package com.pkwiatko.inmemorydbapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InmemorydbAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InmemorydbAppApplication.class, args);
+	}
+
+}
+
